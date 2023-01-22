61b52cf302e00297e20c09d940db92b65e996eff
Инна <anni31717213@gmail.com>
2023-01-23 00:24:16 +0200

    змінила інпут

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,28 +1,49 @@
 import css from './ContactForm.module.css';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
+
 import { addContact } from '../../redux/contactsSlice';
 import { getContacts } from '../../redux/selectors';
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
 
   const handleSubmit = e => {
     e.preventDefault();
-    const form = e.target;
-    const name = form.elements.name.value;
-    const number = form.elements.number.value;
-    form.reset();
+   
     if (contacts.value.find(contact => contact.name.toLowerCase() === name.toLowerCase())) {
       alert(`${name} is already in contacts`);
-      return false;
+      return;
     }
-    dispatch(addContact(name, number));
-    return true;
+    dispatch(addContact({ name, number }));
+    reset();
+  };
+   const handleChange = e => {
+    const currentTarget = e.currentTarget.name;
+    const value = e.currentTarget.value;
+
+    switch (currentTarget) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        throw Error(`CurrentTarget ${currentTarget} is invalid`);
+    }
   };
 
-   
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
   return (
       <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
         <label className={css.label}>
@@ -34,7 +55,9 @@ const ContactForm = () => {
             placeholder="Inna"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
+          required
+          onChange={handleChange}
+          value={name}
           />
         </label>
         <label className={css.label}>
@@ -47,6 +70,8 @@ const ContactForm = () => {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+          onChange={handleChange}
+          value={number}
           />
         </label>
         <button className={css.btn} type="submit">
@@ -56,8 +81,6 @@ const ContactForm = () => {
     );
   }
 
-ContactForm.propTypes = {
-  contacts: PropTypes.object,
-};
+
 
 export default ContactForm;
